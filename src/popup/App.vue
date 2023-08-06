a11df271fbcf854bc72336b0a33aff86cb2b3b18
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2023-08-06 19:06:59 +0600

    chore: setup extension pages

---
+++ src/popup/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class="text-center">Popup Page</h1>
+</template>
+
+<style scoped>
+h1 {
+    color: teal;
+}
+.text-center {
+    text-align: center;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { crx } from '@crxjs/vite-plugin'
+
+//@ts-ignore
+import manifest from './src/manifest'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [vue(), crx({ manifest })],
+    server: {
+        hmr: {
+            host: 'localhost',
+            port: 3000,
+            protocol: 'ws',
+        },
+    },
 })
